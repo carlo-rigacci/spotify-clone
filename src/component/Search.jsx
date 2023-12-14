c3e9bae00d5fc1b18a0b516e1aca2e42258a2641
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-14 19:14:41 +0100

    fix style

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -21,7 +21,10 @@ export default function Search() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col p-7 relative" style={{backgroundColor : "rgba(18,18,18,255)"}}>
+    <div
+      className="min-h-screen flex flex-col p-7 relative"
+      style={{ backgroundColor: "rgba(18,18,18,255)" }}
+    >
       <h1 className="text-white mb-5 text-2xl font-bold">Browse all</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-4">
         {title.map((titleName, index) => {
@@ -29,11 +32,15 @@ export default function Search() {
           return (
             <div
               key={index}
-              className={`spect-square rounded-lg p-5 cursor-pointer text-3xl  overflow-hidden md:text-4xl font-semibold lg:text-3xl`}
+              className={` rounded-xl aspect-squarerounded-lg p-5 cursor-pointer text-3xl  overflow-hidden md:text-4xl font-semibold lg:text-3xl`}
               style={{ backgroundColor: randomColor }}
             >
               {titleName.titolo}
-              <img src={titleName.img} alt={titleName.titolo} className=" bottom-0 transform rotate-45 scale-90 translate-x-1/3 translate-y-1/2 "/>
+              <img
+                src={titleName.img}
+                alt={titleName.titolo}
+                className="transform rotate-45 scale-90 translate-x-1/3 translate-y-1/2 rounded-xl"
+              />
             </div>
           );
         })}
