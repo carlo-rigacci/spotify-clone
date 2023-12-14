4cc55d6cb87ff0721ee2393f21c8afa930593644
Faintful <72162133+Faintful@users.noreply.github.com>
2023-12-14 17:33:11 +0100

    Eliminato doppio componente

--- src/component/Album.jsx
+++
@@ -1,9 +0,0 @@
-export default function Album({ img, title, desc }) {
-  return (
-    <div>
-      <img src={img.src} alt={img.alt} />
-      <h2>{title}</h2>
-      <p>{desc}</p>
-    </div>
-  );
-}
