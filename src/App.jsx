0290cb4607d29c4b0ae376dd1645163e24ffc40e
DHEYNE MARGON GHISOLFI <113483975+Dhemg@users.noreply.github.com>
2023-12-14 15:30:54 +0100

    strutura iniziale

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './component/nav/Navbar';
+import Footer from './component/footer/Footer';
 
-function App() {
+
+export default function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route></Route>
-        </Routes>
+        <Navbar/>             
+          <Routes>
+            <Route/>
+                  
+          </Routes>
       </Router>
+
+      <Footer/>
     </>
   );
 }
 
-export default App;
+
+
