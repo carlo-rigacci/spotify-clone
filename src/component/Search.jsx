3fd15cbf9a577df60f16388e52348d41e2dea830
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-14 17:57:29 +0100

    fix styles

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -6,35 +6,30 @@ export default function Search() {
 
   let endpoint = "src/component/generi.json";
   useEffect(() => {
-    Axios.get(endpoint)
-      .then((res) => {
-        setTitle([...res.data.generi]);
-        console.log(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    Axios.get(endpoint).then((res) => {
+      setTitle([...res.data.generi]);
+    });
   }, []);
 
   const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
+    const letters = "89ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 8)];
     }
     return color;
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col p-7">
-      <h1 className="text-white mb-5 text-2xl">Browse all</h1>
+    <div className="min-h-screen flex flex-col p-7" style={{backgroundColor : "rgba(18,18,18,255)"}}>
+      <h1 className="text-white mb-5 text-2xl font-bold">Browse all</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4">
         {title.map((titleName, index) => {
           const randomColor = getRandomColor();
           return (
             <div
               key={index}
-              className={`card aspect-square rounded-lg p-5`}
+              className={`aspect-square rounded-lg p-5 cursor-pointer text-2xl font-bold text-white`}
               style={{ backgroundColor: randomColor }}
             >
               {titleName.titolo}
