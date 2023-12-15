5b0ecd3892bd3dbc71bdcfc9e7d582a5dca28ab5
Faintful <72162133+Faintful@users.noreply.github.com>
2023-12-15 09:48:54 +0100

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from './component/Container';
-import Playlist from './component/Playlist'
+import Playlist from './component/Playlist';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route>
-          <Route path="/Playlist" element={<Playlist/>} />
-          </Route>
-        </Routes>
-      </Router>
-      <Container/>
-    </>
-  );
+  return <Playlist />;
 }
 
 export default App;
