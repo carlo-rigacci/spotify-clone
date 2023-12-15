77f9d2540daaca2cabc95cff3e30eda93db8d71c
DHEYNE MARGON GHISOLFI <113483975+Dhemg@users.noreply.github.com>
2023-12-15 15:49:21 +0100

    search-bar

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -13,18 +13,16 @@ function Navbar () {
     <nav className='Wrapper'>
 
         {/*pulsanti frecce */}       
-            <div >
+            <div className= 'row'>
+                
                 <button className='arrow1'><IoIosArrowBack /></button>
                 <button className='arrow2'><IoIosArrowForward /></button>
-
+                
                 {/*search */}
-                
-                                 
-                    <input className= 'search' type='text' placeholder='Search...'></input> 
-              
-                
-                
-                                          
+                <div className=' pointer-events-auto flex items-center justify-center bg-[#434343] gap-2 text-sm w-full py-2 px-3 ring-1 ring-slate-900/10 text-slate-500 rounded-2xl'>          
+                <i><CiSearch /></i>
+                <input className= 'search' type='text' placeholder='Search...'></input> 
+                </div>                                     
              
             </div>
 
