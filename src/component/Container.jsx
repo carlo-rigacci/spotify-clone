bc7486670d4c57b723fae5f4bbdc2fde9b9ce390
toukirislam <shovoodev@gmail.com>
2023-12-15 11:34:55 +0100

    change

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -30,19 +30,16 @@ export default function Container() {
           };
           
   return (
-    <div className="flex w-[250px] rounded-lg bg-gray-700 px-2 py-2 hover:opacity-75" onMouseOver={handleMouseOver}
+    <div className="flex w-[250px] rounded-lg bg-gray-700 px-2 py-2 hover:bg-gray-500" onMouseOver={handleMouseOver}
     onMouseOut={handleMouseOut}
     style={{ position: 'relative', display: 'inline-block' }}>
     <div className="max-w-sm rounded overflow-hidden shadow-lg flex-col justify-center items-center">
         <img className="w-[230px] h-[150px]" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"/>
         {isHovered && (
         <button
-        className="text-white bg-green-600 float-right  focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2  "
-          onClick={() => {
-
-        }}
+        className=" even:none text-white bg-green-600 float-right  focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2  "
         >
-            play
+            <i class="fa-solid fa-play"></i>
         </button>
       )}
         <div className="px-3 py-2">
