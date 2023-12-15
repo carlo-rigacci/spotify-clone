7b9eb5f287b81be8c3a033a6513bda9c4292cc20
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-15 11:52:03 +0100

    category page

---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,12 @@
+import Section from "../component/Section"
+
+export default function CategoryPage({dati}){
+    
+
+
+    return (
+        <>
+        <Section dati = {dati}/>
+        </>
+    )
+}
