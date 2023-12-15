3d021fee54fafd48d6efe8d7e4e06bc57f5af329
DHEYNE MARGON GHISOLFI <113483975+Dhemg@users.noreply.github.com>
2023-12-15 15:52:46 +0100

    strutura-finale

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -18,14 +18,13 @@ function Navbar () {
                 <button className='arrow1'><IoIosArrowBack /></button>
                 <button className='arrow2'><IoIosArrowForward /></button>
                 
-                {/*search */}
+                {/*search bar*/}
                 <div className=' pointer-events-auto flex items-center justify-center bg-[#434343] gap-2 text-sm w-full py-2 px-3 ring-1 ring-slate-900/10 text-slate-500 rounded-2xl'>          
                 <i><CiSearch /></i>
                 <input className= 'search' type='text' placeholder='Search...'></input> 
                 </div>                                     
              
             </div>
-
        
       
         {/*pulsanti di accesso */}  
