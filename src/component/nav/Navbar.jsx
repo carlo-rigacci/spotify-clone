50aca862d8caaf56c02869c52da7d96f56230252
DHEYNE MARGON GHISOLFI <113483975+Dhemg@users.noreply.github.com>
2023-12-15 10:21:05 +0100

    navbar-arrow

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -1,5 +1,8 @@
 
 import './Navbar.css'
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
 
 
 export default Navbar
@@ -7,11 +10,18 @@ export default Navbar
 function Navbar () {
     return (           
     <nav className='Wrapper'>
-
-       <button className='btn1'>Sign Up</button>
-       <button className='btn2'>Log In</button>
        
-   
+            <div >
+                <button className='arrow1'><IoIosArrowBack /></button>
+                <button className='arrow2'><IoIosArrowForward /></button>
+            </div>
+
+            <div>
+                <button className='btn1'>Sign Up</button>
+                <button className='btn2'>Log In</button>
+            </div>
+       
+          
     </nav>
     );
 }
