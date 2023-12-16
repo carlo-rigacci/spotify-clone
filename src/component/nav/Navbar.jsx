3ffcf96e1987a8e7a1362748e7580da1715b79bd
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-16 18:31:58 +0100

    fix bg color +rimozione classe wrapper

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -11,7 +11,7 @@ export default Navbar
 
 function Navbar () {
     return (           
-    <nav className='Wrapper flex justify-around items-center sticky z-[100] top-0' style={{ backgroundColor: "rgba(16,16,16,255)" }}>
+    <nav className='Wrapper flex justify-around items-center sticky z-[100] top-0 bg-black'>
 
             {/*pulsanti frecce */}       
             <div className= 'row'>
