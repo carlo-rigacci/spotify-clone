84e175940308fe61cb24205c9bdf07296b6a9d95
Faintful <72162133+Faintful@users.noreply.github.com>
2023-12-17 20:13:16 +0100

    Refactoring

--- src/api/getTokens.js
+++ src/api/getTokens.js
@@ -1,11 +1,11 @@
 import path from 'path';
-import { writeFile } from 'fs';
+import { writeFile } from 'fs/promises';
 
 const endpoint = 'https://accounts.spotify.com/api/token';
 
 const clientId = '2d112e6a83e240c68e5ca1219abdcf79';
 const clientSecret = '28dc47e3130c4d799af5e9cbda237f4d';
-const base64Encoded = btoa(clientId + clientSecret);
+const credentialsBase64Encoded = btoa(clientId + clientSecret);
 
 //access e refresh token entrambi aggiornati
 export var access_token;
@@ -13,20 +13,18 @@ export var refresh_token;
 
 //Funzione che rinnova l'access token tra un ora
 async function refreshToken() {
-  setTimeout(async () => {
-    const response = await fetch(
-      'https://accounts.spotify.com/api/token',
-      httpRequest(
-        'post',
-        'application/x-www-form-urlencoded',
-        `Basic ${base64Encoded}`,
-        `grant_type=refresh_token&refresh_token=${refresh_token}
-    `
-      )
-    );
-    const data = await response.json();
-    access_token = data.access_token;
-  }, 3_600_000); // 1 ora
+  const response = await fetch(
+    'https://accounts.spotify.com/api/token',
+    httpRequest(
+      'post',
+      'application/x-www-form-urlencoded',
+      `Basic ${credentialsBase64Encoded}`,
+      `grant_type=refresh_token&refresh_token=${refresh_token}
+  `
+    )
+  );
+  const data = await response.json();
+  tokenToJSON(data.access_token, 'AccessToken.json');
 }
 
 //Funzione che ritorna una richiesta HTTP
@@ -49,33 +47,26 @@ async function tokenToJson() {
       httpRequest(
         'post',
         'application/x-www-form-urlencoded',
-        'Basic ' + base64Encoded,
+        'Basic ' + credentialsBase64Encoded,
         'grant_type=client_credentials'
       )
     );
 
     const data = await response.json();
-    const accessTokenAsString = JSON.stringify(data.access_token);
-    const refreshTokenAsString = JSON.stringify(data.refresh_token);
+    tokenToJSON(data.access_token, 'AccessToken.json');
+    tokenToJSON(data.refresh_token, 'RefreshToken.json');
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-    const tokenDirectoryPath = './token';
-
-    const accessTokenFileName = 'accessToken.json';
-    const accessTokenPath = path.join(tokenDirectoryPath, accessTokenFileName);
-
-    const refreshTokenFileName = 'refreshToken.json';
-    const refreshTokenPath = path.join(
-      tokenDirectoryPath,
-      refreshTokenFileName
-    );
-
-    writeFile(accessTokenPath, accessTokenAsString, (error) => {
-      console.error(error);
-    });
-
-    writeFile(refreshTokenAsString, accessTokenAsString, (error) => {
-      console.error(error);
-    });
+async function tokenToJSON(json, tokenFileName) {
+  const tokenJSONasString = JSON.stringify(json, tokenFileName);
+  const tokenDirectoryPath = './token';
+  const tokenPath = path.join(tokenDirectoryPath, tokenFileName);
+  try {
+    await writeFile(tokenPath, tokenJSONasString);
+    console.log(tokenFileName + ' written successfully');
   } catch (error) {
     console.error(error);
   }
