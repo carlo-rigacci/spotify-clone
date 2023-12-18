c766dadc463524cc6a13c85856d08a1a8ecfec1d
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-18 14:44:22 +0100

    fix home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,29 @@ import Sidebar from "../component/nav/Sidebar";
 import Secondcontainer from "../component/container/Secondcontainer";
 import SectionContainer from "../component/container/SectionContainer";
 import Navbar from "../component/nav/Navbar";
-import Footer from "../component/footer/Footer"
-
+import Footer from "../component/footer/Footer";
+ 
 export default function Home() {
   return (
-    <div style={{ backgroundColor: "rgba(18,18,18,255)" }}>
-        <div className='flex justify-between '>
-          <div className=" w-[300px]"><Sidebar /></div>
-        <div className='w-[100%]'><Navbar/>
-        <div className='flex-col justify-start'>
-          <SectionContainer/>
-          <Secondcontainer/>
+    <div
+      className="flex flex-col justify-center items-center"
+      style={{ backgroundColor: "rgba(18,18,18,255)" }}
+    >
+      <div className="flex justify-between ">
+        <div className=" w-[300px]">
+          <Sidebar />
         </div>
+        <div className="">
+          <Navbar />
+          <div className="flex-col justify-start">
+            <SectionContainer />
+            <Secondcontainer />
+          </div>
         </div>
-        
-        </div>
-        <Footer/>
+      </div>
+      <div className="">
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
