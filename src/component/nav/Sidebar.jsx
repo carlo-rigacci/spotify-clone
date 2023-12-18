bdd02d7ca090e07c5477b267ad912af3c290f2bf
Erry002 <enricogarozzo002@gmail.com>
2023-12-18 10:38:57 +0100

    Correxioni sidebar

--- src/component/nav/Sidebar.jsx
+++ src/component/nav/Sidebar.jsx
@@ -6,7 +6,7 @@ import LanguageModal from "../modal/LanguageModal.jsx";
 export default function Sidebar() {
   return (
     <nav className=" bg-black flex flex-col sticky top-0">
-      <div className="  flex flex-col w-[300px] h-sceen">
+      <div className="  flex flex-col w-[300px]">
         <div className=" bg-[#121212] flex flex-col m-2 rounded-md gap-3">
           <div
             className=" pl-4 pt-5
@@ -64,14 +64,14 @@ export default function Sidebar() {
             <div className="playlist h-fit w-full mt-10 p-4 rounded-xl text-white"style={{ backgroundColor: "rgba(36,36,36,255)" }}>
               <h1 className=" mb-2">Create your first playlist</h1>
               <h3 className="mb-6">It's easy, we'll help you</h3>
-              <button className="text-black bg-white p-3 rounded-full text-md font-bold">Create playlist</button>
+              <button className="text-black bg-white p-3 rounded-full  hover:scale-110 text-md font-bold">Create playlist</button>
             </div>
             <div className="playlist h-fit w-full mt-10 p-4 rounded-xl text-white"style={{ backgroundColor: "rgba(36,36,36,255)" }}>
               <h1 className=" mb-2">Let's find some podcasts to follow</h1>
               <h3 className="mb-6">We'll keep you updated on new episodes</h3>
-              <button className="text-black bg-white p-3 rounded-full text-md font-bold">Browse podcasts</button>
+              <button className="text-black bg-white p-3 rounded-full hover:scale-110 text-md font-bold">Browse podcasts</button>
             </div>
-            <div className="flex flex-col items-center justify-center relative top-[430px]">
+            <div className="flex flex-col items-center justify-center relative top-[130px]">
               <footer className="flex flex-col items-center bg-[#121212] p-2 w-[280px]">
                 <div className="grid grid-cols-2 gap-2 text-xs">
                   <p className="underline cursor-pointer">Legal information</p>
