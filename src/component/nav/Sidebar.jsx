7387b82eb4fac2019e69ddc579d96eebc5ff5395
Giacomo Giraldi <ggiraldi18@gmail.com>
2023-12-18 14:45:25 +0100

    fix sidebar

--- src/component/nav/Sidebar.jsx
+++ src/component/nav/Sidebar.jsx
@@ -40,7 +40,7 @@ export default function Sidebar() {
           </div>
         </div>
         <Wrapper>
-          <div className="flex flex-col items-center fill-[#b8b8b8] text-[#b8b8b8] transition-all pt-5 ml-2 mr-2 mb-2">
+          <div className="flex flex-col items-center fill-[#b8b8b8] text-[#b8b8b8] transition-all pt-5 ml-2  mr-2 mb-2">
             <div className="flex items-center hover:fill-white hover:text-white">
               <button className="flex flex-row gap-3">
                 <span>
@@ -61,18 +61,28 @@ export default function Sidebar() {
                 </span>
               </button>
             </div>
-            <div className="playlist h-fit w-full mt-10 p-4 rounded-xl text-white"style={{ backgroundColor: "rgba(36,36,36,255)" }}>
+            <div
+              className="playlist w-full mt-10 p-4 rounded-xl text-white"
+              style={{ backgroundColor: "rgba(36,36,36,255)" }}
+            >
               <h1 className=" mb-2">Create your first playlist</h1>
               <h3 className="mb-6">It's easy, we'll help you</h3>
-              <button className="text-black bg-white p-3 rounded-full  hover:scale-110 text-md font-bold">Create playlist</button>
+              <button className="text-black bg-white p-3 rounded-full  hover:scale-110 text-md font-bold">
+                Create playlist
+              </button>
             </div>
-            <div className="playlist h-fit w-full mt-10 p-4 rounded-xl text-white"style={{ backgroundColor: "rgba(36,36,36,255)" }}>
+            <div
+              className="playlist w-full mt-10 p-4 rounded-xl text-white"
+              style={{ backgroundColor: "rgba(36,36,36,255)" }}
+            >
               <h1 className=" mb-2">Let's find some podcasts to follow</h1>
               <h3 className="mb-6">We'll keep you updated on new episodes</h3>
-              <button className="text-black bg-white p-3 rounded-full hover:scale-110 text-md font-bold">Browse podcasts</button>
+              <button className="text-black bg-white p-3 rounded-full hover:scale-110 text-md font-bold">
+                Browse podcasts
+              </button>
             </div>
-            <div className="flex flex-col items-center justify-center relative top-[0px]">
-              <footer className="flex flex-col items-center bg-[#121212] p-2 w-[280px]">
+            <div className="flex flex-col h-fit items-center justify-center relative top-[5px]">
+              <footer className="flex flex-col h-fit items-center bg-[#121212] p-2 w-[280px]">
                 <div className="grid grid-cols-2 gap-2 text-xs">
                   <p className="underline cursor-pointer">Legal information</p>
                   <p className="underline cursor-pointer">Privacy Center</p>
