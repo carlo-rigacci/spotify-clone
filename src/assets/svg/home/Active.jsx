c9876e9938306bb0c3c6989f0b24668b74d0d86f
Faintful <72162133+Faintful@users.noreply.github.com>
2023-12-23 01:48:36 +0100

    Home active/inactive svg

---
+++ src/assets/svg/home/Active.jsx
@@ -0,0 +1,13 @@
+export default function Active() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      tabIndex='0'
+      viewBox='0 0 24 24'
+      fill='white'
+      height='30'
+    >
+      <path d='M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8' fill='white'></path>
+    </svg>
+  );
+}
